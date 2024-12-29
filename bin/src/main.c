8698b2de34b82934ce14551efc024ad5a3b19cbc
Zopnote <lennysiebert08@gmail.com>
2024-12-29 19:47:23 +0100

    Updated build system by adding lib and subprojects

--- bin/src/main.c
+++ bin/src/main.c
@@ -1,24 +1,26 @@
 #include <cthreads/cthreads.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-// Die Funktion, die im neuen Thread ausgeführt wird
+
 void* thread_function(void* arg) {
     printf("Thread läuft\n");
     return NULL;
 }
+
 int main() {
-    // 1. Deklaration einer Thread-Referenz
+    printf("Executed successfull.\n");
     struct cthreads_thread my_thread;
 
-    // 2. Erstellen des Threads und Zuweisen der Referenz
-    struct cthreads_args args;  // Argumente für den Thread (optional, hier nicht verwendet)
+    struct cthreads_args args = {0};
+
     int result = cthreads_thread_create(&my_thread, NULL, thread_function, NULL, &args);
 
     if (result != 0) {
         printf("Fehler beim Erstellen des Threads\n");
         return 1;
     }
-
-    // 3. Optional: Den Thread abtrennen, falls du ihn nicht später joinen willst
     cthreads_thread_detach(my_thread);
-};
+    while (1);
+    return 1;
+} 
