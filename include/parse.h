b07f6aa950cd79f38323f4d4fb684b3a0aaf9c3f
Zopnote <lennysiebert08@gmail.com>
2025-02-12 21:59:13 +0100

    Added documentation

--- include/parse.h
+++ include/parse.h
@@ -12,36 +12,85 @@
     #define EXTERN __attribute__((visibility("default")))
 #endif
 
+
+/**
+ * @brief Available configuration types of the parse collection.
+ */
 enum {
     yaml,
     json
 } typedef ParseFormat;
 
-
+/**
+ * @brief The type the buffer of an entry has.
+ *
+ * Maps are further ParseEntry arrays which must be
+ * already defined and set as buffer of an entry to store
+ * the recursive values in it.
+ *
+ * Lists are an array of unparsed strings of the parsed document.
+ * Their buffer should not be set at first definition.
+ */
 enum {
-    entries,
+    map,
     integer,
     string,
     floating,
     list
 } typedef ParseType;
 
+
+/**
+ * @brief Representation of an entry.
+ *
+ * Define a tree by create an array of entries to load
+ * configuration values from a file or write them to a string.
+ *
+ * All value buffers except the map should not be set.
+ * Maps are further ParseEntry arrays. The size value represent
+ * the length of an array in case the buffer is an array
+ * or the actual size of the value behind the buffer.
+ *
+ * The parser scans for the key and decide based on the ParseType
+ * what the buffer should be and set it. Buffers can be null even after
+ * the parse process, if the value was not found.
+ */
 struct {
     char* key;
     ParseType type;
     void* buffer;
-    size_t bufferSize;
+    size_t size;
 } typedef ParseEntry;
 
-EXTERN void parse_resolveString(
-    const char* string,
-    const ParseFormat format,
-    const size_t entriesBufferSize,
-    ParseEntry entriesBuffer[]
+/**
+ * @brief Initialize the yaml parser with the input string and continues with the scan.
+ *
+ * The entries array will be filled with the found values.
+ */
+EXTERN void parse_resolveYamlString(
+    const char* const string,
+    ParseEntry* const entries,
+    const size_t entriesLength,
+    const bool verbose
 );
+
+/**
+ * @brief Initialize the json parser with the input string and continues with the scan.
+ *
+ * The entries array will be filled with the found values.
+ */
+EXTERN void parse_resolveJsonString(
+    const char* const string,
+    ParseEntry* const entries,
+    const size_t entriesLength
+);
+
+/**
+ * @brief Writes the entries to the string buffer with the desired configuration format.
+ */
 EXTERN void parse_emitString(
     char* stringBuffer,
     const ParseFormat format,
-    const size_t entriesBufferSize,
-    ParseEntry entriesBuffer[]
+    const ParseEntry* const entries,
+    const size_t entriesLength
 );
