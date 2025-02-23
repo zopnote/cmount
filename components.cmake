015e92fb0f68fcff8d0755c7505f5309f7d4238b
Zopnote <lennysiebert08@gmail.com>
2025-02-23 15:39:43 +0100

    Reduced complexity of cmake configuration by summarize scripts into CMakeLists.txt

--- components.cmake
+++
@@ -1,4 +0,0 @@
-set(SRC_COMPONENT_DIRECTORY "${CMAKE_SOURCE_DIR}/src")
-include("${SRC_COMPONENT_DIRECTORY}/core.cmake")
-include("${SRC_COMPONENT_DIRECTORY}/parse.cmake")
-include("${SRC_COMPONENT_DIRECTORY}/main.cmake")
