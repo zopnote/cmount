510315f514a3a3e11faffc651268eef68ecfa50b
Zopnote <lennysiebert08@gmail.com>
2025-03-21 13:07:37 +0100

    Added main test

--- src/main.cmake
+++ src/main.cmake
@@ -1,6 +1,15 @@
 
 set(SRC_FILES_MAIN_EXECUTABLE
         ${SRC_COMPONENT_DIRECTORY}/main/main.c
+        ${SRC_COMPONENT_DIRECTORY}/main/tasks/build.c
+        ${SRC_COMPONENT_DIRECTORY}/main/tasks/config.c
+        ${SRC_COMPONENT_DIRECTORY}/main/tasks/create.c
+        ${SRC_COMPONENT_DIRECTORY}/main/tasks/doctor.c
+        ${SRC_COMPONENT_DIRECTORY}/main/tasks/get.c
+        ${SRC_COMPONENT_DIRECTORY}/main/tasks/install.c
+        ${SRC_COMPONENT_DIRECTORY}/main/tasks/run.c
+        ${SRC_COMPONENT_DIRECTORY}/main/tasks/upgrade.c
+        ${SRC_COMPONENT_DIRECTORY}/main/tasks/version.c
 )
 add_executable(${PROJECT_NAME} ${SRC_FILES_MAIN_EXECUTABLE})
 
---
+++ src/main/tasks/create.c
@@ -0,0 +1,3 @@
+#include "../tasks.h"
+
+bool create()
